9b4cc40ce2fadc4bfdf906523d1627d926bc1817
Hans Klunder <hans.klunder@xs4all.nl>
2018-04-14 19:47:20 +0200

    Added LRU-cache backend

---
+++ backends/lru-cache.js
@@ -0,0 +1,49 @@
+const LruCache = require("lru-cache");
+const Config = {
+  LruConfig: 50,
+  Logger: console.log,
+  Version: 1
+};
+
+class LRU {
+  constructor(cfg) {
+    this.config = Object.assign(Config, cfg);
+    this.cache = LruCache(this.config.LruConfig);
+  }
+
+  // the memcached commands
+  get(p) {
+    // translate single request into multiple promises, 1 for each key to get
+    const Proms = p.keys.map(key => {
+      return new Promise((resolve, reject) => {
+        const value = this.cache.get(key);
+        resolve({ key, value });
+      });
+    });
+    return Promise.all(Proms);
+  }
+
+  set(p) {
+    return new Promise((resolve, reject) => {
+      console.log("setting item", p.key, p.data);
+      this.cache.set(p.key, p.data);
+      resolve(true);
+    });
+  }
+
+  delete(p) {
+    return new Promise((resolve, reject) => {
+      this.cache.del(p.key);
+      resolve(false);
+    });
+  }
+
+  version() {
+    return new Promise((resolve, reject) => {
+      resolve(this.config.Version);
+    });
+  }
+}
+
+module.exports = config => new LRU(config);
+module.exports.LRU = LRU;
---
+++ test/testLru-Cache.js
@@ -0,0 +1,45 @@
+const LRU = require("../backends/lru-cache");
+const MC = require("../memcachedproxy");
+const Memcached = require("memcached");
+const Net = require("net");
+// server config
+
+const MaxRequestSize = 1024;
+const Logger = _ => {};
+// Lru LruCache
+const LruConfig = 50;
+
+const test = require("tap").test;
+
+test("test can use LRU-cache with memcache driver!", function(t) {
+  const server = Net.createServer(
+    MC({
+      MaxRequestSize,
+      Backend: LRU({
+        LruConfig,
+        Logger
+      })
+    })
+  );
+  server.listen(0, function() {
+    var memcached = new Memcached("localhost:" + server.address().port);
+
+    var v = Date.now().toString();
+    memcached.set("a", v, 1, function(err, data) {
+      t.ok(!err, "set should not have error");
+
+      memcached.get("a", function(err, value) {
+        t.ok(!err, "get should not have error");
+        t.equals(value, v, "should return the correct value");
+
+        memcached.del("b", function(err, value) {
+          t.ok(!err, "delete should not have error");
+          memcached.end();
+          server.close();
+
+          t.end();
+        });
+      });
+    });
+  });
+});
