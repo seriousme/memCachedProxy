af1a98c3166e3b448a6ea797aaba57cdf28f050c
Hans Klunder <hans.klunder@xs4all.nl>
2018-04-14 14:00:24 +0200

    cleanup

--- backends/dynamoMC.js
+++ backends/dynamoMC.js
@@ -3,7 +3,9 @@ const Config = {
   Table: "myCacheTable",
   KeyAttr: "myKeyAttribute",
   ValueAttr: "myValueAttribute",
-  AWSregion: "eu-west-1"
+  AWSregion: "eu-west-1",
+  Logger: console.log,
+  Version: 1
 };
 
 class DynamoMC {
@@ -15,31 +17,38 @@ class DynamoMC {
     });
     this.DocClient = new AWS.DynamoDB.DocumentClient();
   }
+
+  _log(l, msg) {
+    this.config.Logger({ level: l, msg });
+  }
+
   _request(operation, params) {
-    console.log("request", { operation, params });
+    this._log("DEBUG", `request ${JSON.stringify({ operation, params })}`);
     return new Promise((resolve, reject) => {
       this.DocClient[operation](params, (err, data) => {
         if (err) {
-          reject(
+          this._log(
+            "DEBUG",
             `Unable to ${operation} item with params: ${JSON.stringify(
               params,
               null,
               2
             )} Error JSON ${JSON.stringify(err, null, 2)}`
           );
+          reject();
         } else {
-          console.log("received1", data);
-          if (
-            operation == "get" &&
-            data.Item &&
-            data.Item[this.config.ValueAttr]
-          ) {
-            data = {
-              key: params.Key[this.config.KeyAttr],
-              value: data.Item[this.config.ValueAttr]
-            };
+          this._log("DEBUG", `received ${JSON.stringify(data)}`);
+          if (operation == "get") {
+            if (data.Item && data.Item[this.config.ValueAttr]) {
+              data = {
+                key: params.Key[this.config.KeyAttr],
+                value: data.Item[this.config.ValueAttr]
+              };
+            }
+            resolve(data);
+          } else {
+            resolve(true);
           }
-          resolve(data);
         }
       });
     });
@@ -53,6 +62,7 @@ class DynamoMC {
     Params.Key[this.config.KeyAttr] = p.key;
     return this._request("get", Params);
   }
+
   set(p) {
     const Params = {
       TableName: Config.Table
@@ -61,6 +71,12 @@ class DynamoMC {
     Params.Item[this.config.KeyAttr] = p.key;
     Params.Item[this.config.ValueAttr] = p.data;
     return this._request("put", Params);
+  }
+
+  version() {
+    return new Promise((resolve, reject) => {
+      resolve(this.config.Version);
+    });
   }
 }
 
