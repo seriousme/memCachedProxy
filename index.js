e22f330822696cbde48fa6fc00534f94a612f116
Hans Klunder <hans.klunder@xs4all.nl>
2018-04-14 20:21:55 +0200

    make lru-cache the default

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DynamoDB = require("./backends/DynamoDB");
+const LRU = require("./backends/lru-cache");
 const MemCachedServer = require("./memCachedProxy");
 
 const Net = require("net");
@@ -6,20 +6,12 @@ const Net = require("net");
 const Port = 11200;
 
 const MaxRequestSize = 1024;
-// DynamoDB
-const Table = "userData";
-const KeyAttr = "userId";
-const ValueAttr = "pwd";
-const AWSregion = "eu-west-1";
+// LRU
+const LruConfig = 50;
 
 const server = Net.createServer(
   MemCachedServer({
     MaxRequestSize,
-    Backend: DynamoDB({
-      Table,
-      KeyAttr,
-      ValueAttr,
-      AWSregion
-    })
+    Backend: LRU({ LruConfig })
   })
 ).listen(Port, _ => console.log(`Server started on port ${Port}`));
